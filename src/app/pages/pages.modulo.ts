b07c61ea97f4d56bc8e9b80e8599182fce0c0e19
Jorge Cardenas <jfcardenas@tc.tecun.net.gt>
2018-08-06 15:33:47 -0600

    input, output y viewchild

--- src/app/pages/pages.modulo.ts
+++ src/app/pages/pages.modulo.ts
@@ -4,7 +4,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
+import { FormsModule } from '@angular/forms';
 import { PAGES_ROUTES } from './pages.routes';
+// ng2 charts
+import { ChartsModule } from 'ng2-charts';
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 
@@ -13,7 +19,9 @@ import { PAGES_ROUTES } from './pages.routes';
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent
   ],
   exports: [
     DashboardComponent,
@@ -22,7 +30,9 @@ import { PAGES_ROUTES } from './pages.routes';
   ],
   imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
   ]
 })
 
